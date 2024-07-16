60961203b9a1b3432dc538613edf0e7348cc29e3
jezvgg <jezv@yandex.ru>
2024-07-16 20:46:47 +1100

    add configfile and autosave last parameters

--- gpt.py
+++ gpt.py
@@ -4,13 +4,26 @@ from g4f import Model
 from prettier import Prettier
 from rich.console import Console
 import click
+import configparser
+
+
+config = configparser.ConfigParser(allow_no_value=True)
+config.read('./config.ini')
+default_provider = config.get('DEFAULT', 'provider') if config.get('DEFAULT', 'provider') else None
+default_model = config.get('DEFAULT', 'model')
 
 
 @click.command()
 @click.option('--answer', prompt=True, required=True)
-@click.option('--provider', type=click.Choice(Provider.__all__+[None]), default=None)
-@click.option('--model', type=click.Choice(Model.__all__()), default='gpt-3.5-turbo')
+@click.option('--provider', type=click.Choice(Provider.__all__+[None]), default=default_provider)
+@click.option('--model', type=click.Choice(Model.__all__()), default=default_model)
 def ask_gpt(answer: str, provider: str, model: str):
+
+    config.set('DEFAULT', 'provider', provider)
+    config.set('DEFAULT', 'model', model)
+    with open('./config.ini', 'w') as file:
+        config.write(file)
+
     client = Client()
     console = Console()
     prettier = Prettier(console)
